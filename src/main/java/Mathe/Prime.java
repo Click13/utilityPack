8b5eb66c7f1be8032a5c4f4d801015905e82e458
MarkusHartel <Markus_H.TT@t-online.de>
2021-11-19 12:08:24 +0100

    neue Klasse überprüft eine Zahl, ob diese eine Primzahl ist

---
+++ src/main/java/Mathe/Prime.java
@@ -0,0 +1,22 @@
+package Mathe;
+
+public class Prime {
+    public static boolean isPrimeNumber(int number){
+        for (int i=2;i<number;i++){
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int[] isPrimeNumber(int[] array){
+        int[] out = new int[array.length];
+
+        for(int i=0;i<array.length;i++){
+            if (isPrimeNumber(array[i])){
+                out[i] = array[i];
+            }
+        }
+        return out;
+    }
+}
