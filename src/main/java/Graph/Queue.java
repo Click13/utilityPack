a1467f87577b62508886b52827323f103d60f974
MarkusHartel <Markus_H.TT@t-online.de>
2021-11-27 14:05:46 +0100

    Graph mit allen Abhängigkeiten hinzugefügt

---
+++ src/main/java/Graph/Queue.java
@@ -0,0 +1,168 @@
+package Graph;
+
+public class Queue {
+    private class Item{
+
+        private Vertex vertex;
+        private Item nextItem;
+
+        public Item(Vertex vertex){
+            this.vertex = vertex;
+        }
+
+        public Vertex getPayload(){
+            return vertex;
+        }
+
+        public void setNextItem(Item nextItem){
+            this.nextItem = nextItem;
+        }
+
+        public Item getNextItem(){
+            return nextItem;
+        }
+    }
+
+    private Item head;
+    private boolean isOrdered;
+
+    public Queue(boolean isOrdered){
+        this.isOrdered = isOrdered;
+        head = null;
+    }
+
+    public boolean isOrdered(){
+        return isOrdered;
+    }
+
+    public boolean isEmpty(){
+        if (head == null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean enqueue(Vertex vertex){
+        if (head == null){
+            head = new Item(vertex);
+            return true;
+        }
+        else {
+            if (contains(vertex)){
+                return false;
+            }
+            else{
+                Item pointer = head;
+                while(pointer.getNextItem() != null){
+                    pointer = pointer.getNextItem();
+                }
+                Item newItem = new Item(vertex);
+                pointer.setNextItem(newItem);
+                if (isOrdered()){
+                    bulkInsert(orderQueue());
+                }
+                return true;
+            }
+        }
+    }
+
+    private void bulkInsert(Item[] items){
+        head = items[0];
+        Item pointer = head;
+        for (int i = 1; i < items.length; i++){
+            pointer.setNextItem(items[i]);
+            pointer = pointer.getNextItem();
+        }
+    }
+
+    private Item[] orderQueue(){
+        Vertex[] verticies = new Vertex[size()];
+        Item pointer = head;
+        for (int i = 0; i < verticies.length; i++){
+            verticies[i] = pointer.getPayload();
+            pointer = pointer.getNextItem();
+        }
+        for (int i = 0; i < verticies.length; i++){
+            tausche(posOf(minimum(i, verticies), verticies), i, verticies);
+        }
+        Item[] out = new Item[verticies.length];
+        for (int i = 0; i < verticies.length; i++){
+            out[i] = new Item(verticies[i]);
+        }
+        return out;
+    }
+
+    private Vertex[] tausche(int a, int b, Vertex[] verticies){
+        Vertex tmp = verticies[a];
+        verticies[a] = verticies[b];
+        verticies[b] = tmp;
+        return verticies;
+    }
+
+    private float minimum(int start, Vertex[] verticies){
+        float min = verticies[start].getDistance();
+        for (int i = 0; i < verticies.length; i++){
+            if (verticies[i].getDistance() < min){
+                min = verticies[i].getDistance();
+            }
+        }
+        return min;
+    }
+
+    private int posOf(float distanz, Vertex[] verticies){
+        int out = 0;
+        for (int i = 0; i < verticies.length; i++){
+            if (verticies[i].getDistance() == distanz){
+                out = i;
+            }
+        }
+        return out;
+    }
+
+    public boolean contains(Vertex vertex){
+        Item pointer = head;
+        while (pointer.getNextItem() != null){
+            if (pointer.getPayload().equals(vertex)){
+                return true;
+            }
+            pointer = pointer.getNextItem();
+        }
+        return false;
+    }
+
+    public boolean dequeue(){
+        if (head == null){
+            return false;
+        }
+        head = head.getNextItem();
+        return true;
+    }
+
+    public Vertex getHead(){
+        if (head == null){
+            return null;
+        }
+        else{
+            return head.getPayload();
+        }
+    }
+
+    public void empty(){
+        head = null;
+    }
+
+    public int size(){
+        int out = 0;
+        Item pointer = head;
+        if (isEmpty()){
+            return out;
+        }
+        while(pointer.getNextItem() != null){
+            out++;
+            pointer = pointer.getNextItem();
+        }
+        return out;
+    }
+}
---
+++ src/main/java/Graph/Vertex.java
@@ -0,0 +1,84 @@
+package Graph;
+
+import java.util.LinkedList;
+
+public class Vertex {
+
+    private String label;
+    private LinkedList<Vertex> adjVertices;
+    private float distance;
+    int x;
+    int y;
+
+    public Vertex(String label, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.label = label;
+        adjVertices = new LinkedList<>();
+        adjVertices.clear();
+    }
+
+    public void setLabel(String label){
+        this.label = label;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+
+    public LinkedList<Vertex> getAdjVertices(){
+        return adjVertices;
+    }
+
+    public boolean addEdgetoVertex(Vertex vertex){
+        if (adjVertices.contains(vertex)){
+            return false;
+        }else{
+            adjVertices.add(vertex);
+            return true;
+        }
+    }
+
+    public boolean removeEdgeToVertex(Vertex vertex){
+        if (adjVertices.contains(vertex)){
+            adjVertices.remove(vertex);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public float getDistance(){
+        return distance;
+    }
+
+    public void setDistance(float distance){
+        this.distance = distance;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public boolean equals(Vertex vertex){
+        if (vertex.getLabel().equals(this.label) && this.x == vertex.getX() && this.y == vertex.getY()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
