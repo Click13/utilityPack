28407965eaadc54a46e65a095e6a3b1d1cad357e
MarkusHartel <Markus_H.TT@t-online.de>
2021-12-02 20:29:07 +0100

    Upgrade zu Version 1.6

--- src/main/java/Datenbanken/EasyAccess.java
+++ src/main/java/Datenbanken/EasyAccess.java
@@ -4,9 +4,12 @@ import java.sql.*;
 
 public class EasyAccess {
 
+    private static EasyAccess instanz = null;
     private Connection connection;
+    private String user;
+    private String password;
 
-    public EasyAccess(String user, String password){
+    private EasyAccess(String user, String password){
         try {
             Class.forName("org.hsqldb.jdbcDriver");
             connection = DriverManager.getConnection("jdbc:hsqldb:file:C:/jbb/base.db;shutdown=true", user, password);
@@ -14,10 +17,13 @@ public class EasyAccess {
         catch (ClassNotFoundException | SQLException e){
             System.out.println("Treiber nicht gefunden" + e.toString());
             System.err.println("Keine Treiberklasse gefunden.");
+            e.printStackTrace();
         }
+        this.user = user;
+        this.password = password;
     }
 
-    public void updateDatabase(String statement){
+    public synchronized void updateDatabase(String statement){
         try {
             PreparedStatement prepared = connection.prepareStatement(statement);
             prepared.executeUpdate();
@@ -32,7 +38,7 @@ public class EasyAccess {
         }
     }
 
-    public ResultSet queryDatabase(String statement){
+    public synchronized ResultSet queryDatabase(String statement){
         ResultSet set = null;
         try{
             PreparedStatement prepared = connection.prepareStatement(statement);
@@ -41,7 +47,42 @@ public class EasyAccess {
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+        try{
+            connection.commit();
+        }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
         return set;
     }
 
+    public synchronized void close(){
+        try {
+            if (!connection.isClosed()){
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        instanz = null;
+    }
+
+    public static synchronized EasyAccess getInstanz(String user, String password){
+        if (instanz == null){
+            instanz = new EasyAccess(user, password);
+            return instanz;
+        }
+        else{
+            if (instanz.user.equals(user) && instanz.password.equals(password)){
+                return instanz;
+            }
+            else{
+                return null;
+            }
+        }
+    }
 }
